57c30db5ebc16824da981d546b5dec9c1110e78a
Hooriya <omer@mac22s-iMac.local>
2014-08-06 10:33:44 +0500

    initial commit

---
+++ MergeVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  MergeVideoViewController.h
+//  videoplayrecord
+//
+//  Created by Omer on 7/24/14.
+//  Copyright (c) 2014 Javaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MergeVideoViewController : UIViewController
+
+@end
---
+++ MergeVideoViewController.m
@@ -0,0 +1,43 @@
+//
+//  MergeVideoViewController.m
+//  videoplayrecord
+//
+//  Created by Omer on 7/24/14.
+//  Copyright (c) 2014 Javaid. All rights reserved.
+//
+
+#import "MergeVideoViewController.h"
+
+@interface MergeVideoViewController ()
+
+@end
+
+@implementation MergeVideoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
